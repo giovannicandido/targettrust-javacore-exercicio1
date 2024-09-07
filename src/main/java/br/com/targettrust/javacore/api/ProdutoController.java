29976bfce5b18c99a4719bed533b52cc7928e9e1
Giovanni Silva <git@gsilva.pro>
2024-09-06 21:45:09 -0300

    cometario sobre o exercicio

--- src/main/java/br/com/targettrust/javacore/api/ProdutoController.java
+++ src/main/java/br/com/targettrust/javacore/api/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+// Toda entrada e saida de informação do sistema passa pelo controlador
 @Component
 public class ProdutoController {
     private final ProdutoService produtoService;
@@ -16,10 +17,15 @@ public class ProdutoController {
         this.produtoService = produtoService;
     }
 
+    // Sempre que a aplicação iniciar esse metodo é executado
     @PostConstruct
     public void init() {
         Produto eletronico1 = new Eletronico(null,"testeeletronico", BigDecimal.valueOf(25.67));
         produtoService.save(eletronico1);
         produtoService.listAll().forEach(System.out::println);
     }
+
+    // TODO implementar o caso de uso do exercicio 2 do material 10
+    // calcular desconto dos produtos no model e passar para o service persistir o novo preço
+    // cada metodo do controlador é um caso de uso/feature ou interação com a UI
 }
